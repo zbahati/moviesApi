00aa01f8550cf781556f007ebbe8ccaf65552f4a
zbahati <bahatizirimwabagabo@gmai.com>
2023-05-31 09:59:14 +0200

    Add comment

--- src/modules/getmovies.js
+++ src/modules/getmovies.js
@@ -2,7 +2,13 @@ const getImage = () => {
   const imageContainer = document.querySelector('.movies-banners');
 
   fetch('https://api.tvmaze.com/shows?page=2')
-    .then((response) => response.json())
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error(`Error fetching data: ${response.status}`);
+      }
+    })
     .then((data) => {
       data.forEach((element) => {
         const markup = `
@@ -23,10 +29,9 @@ const getImage = () => {
               </div>
             </div>
           </div>`;
+
         imageContainer.insertAdjacentHTML('beforeend', markup);
       });
-
-   
 
       const thumbsUpButtons = document.querySelectorAll('.thumbsUpBtn');
       thumbsUpButtons.forEach((button) => {
@@ -34,15 +39,18 @@ const getImage = () => {
           const itemId = button.parentElement.parentElement.querySelector('.comment').getAttribute('data-id');
 
           fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/likes/`)
-            .then((response) => response.json())
+            .then((response) => {
+              if (response.ok) {
+                return response.json();
+              } else {
+                throw new Error(`Error fetching likes data: ${response.status}`);
+              }
+            })
             .then((data) => {
               const itemLikes = data.find((item) => item.item_id === itemId);
-
-              if (itemLikes) {
-                const totalLikesElement = document.getElementById(`total-likes-${itemId}`);
-                const totalLikes = itemLikes.likes || 0;
-                totalLikesElement.textContent = totalLikes;
-              }
+              const totalLikesElement = document.getElementById(`total-likes-${itemId}`);
+              const totalLikes = itemLikes ? itemLikes.likes || 0 : 0;
+              totalLikesElement.textContent = totalLikes;
 
               fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/likes/`, {
                 method: 'POST',
@@ -54,9 +62,8 @@ const getImage = () => {
                 .then((response) => {
                   if (response.ok) {
                     console.log('Like recorded successfully!');
-                    updateTotalLikes();
                   } else {
-                    console.error('Failed to record like:', response.status);
+                    throw new Error(`Failed to record like: ${response.status}`);
                   }
                 })
                 .catch((error) => {
@@ -64,14 +71,159 @@ const getImage = () => {
                 });
             })
             .catch((error) => {
-              console.error('Error retrieving data:', error);
+              console.error('Error retrieving likes data:', error);
             });
         });
       });
 
+      initializeComments(data);
       updateTotalLikes();
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
     });
+
+    window.onload = updateTotalLikes;
 };
+
+
+
+
+
+const initializeComments = (data) => {
+  const commentButtons = document.querySelectorAll('.comment');
+
+  commentButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const popup = document.querySelector('#popup-modal');
+      const movieId = button.getAttribute('data-id');
+      const relatedItem = data.find((item) => item.id === parseInt(movieId));
+
+      popup.innerHTML = '';
+
+      const popupMarkup = `
+        <div class="img">
+          <div class="movies-details-section">
+            <img src="${relatedItem.image.original}" alt="movies images">
+            <span id="close-btn">&#10005;</span>
+            <div class="summary">
+              <p>
+                Name: ${relatedItem.name} &nbsp;&nbsp;&nbsp; Genres: ${relatedItem.genres} <br>
+                Country: ${relatedItem.network.country.name} &nbsp;&nbsp;&nbsp; Language: ${relatedItem.language} <br>
+                Released Date: ${relatedItem.premiered} &nbsp;&nbsp;&nbsp; Status: ${relatedItem.status}<br>
+              </p>
+            </div>
+          </div>
+          <div class="description">
+          <div class="comment-count">
+           <p>Comment: <span id="comment-count-${movieId}">0</span> <p>
+        </div>
+            <div class="added-comment">
+              <!-- Display the comments from the database -->
+              <div id="comment-list-${movieId}"></div>
+            </div>
+          </div>
+          <div class="comment-section">
+            <form id="form-${movieId}">
+              <input type="text" name="name" placeholder="Your Name">
+              <textarea name="message" placeholder="Your Insights"></textarea>
+              <button type="submit">Comment</button>
+            </form>
+
+          </div>
+        </div>`;
+
+      popup.insertAdjacentHTML('beforeend', popupMarkup);
+
+      popup.style.display = 'block';
+
+      const closeBtn = document.querySelector('#close-btn');
+      closeBtn.addEventListener('click', () => {
+        popup.style.display = 'none';
+      });
+
+      const form = document.getElementById(`form-${movieId}`);
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(form);
+        const name = formData.get('name');
+        const message = formData.get('message');
+
+        const commentData = {
+          item_id: relatedItem.id,
+          username: name,
+          comment: message,
+        };
+
+        fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/comments/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(commentData),
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log('Comment posted successfully!');
+
+              getComments(movieId);
+
+              getCommentCount(movieId);
+            } else {
+              throw new Error(`Failed to post comment: ${response.status}`);
+            }
+          })
+          .catch((error) => {
+            console.error('Error posting comment:', error);
+          });
+      });
+
+
+      getComments(movieId);
+
+      getCommentCount(movieId);
+    });
+  });
+};
+
+const getComments = async (movieId) => {
+  try {
+    const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/comments/?item_id=${movieId}`);
+    if (!response.ok) {
+      throw new Error(`Error fetching comments: ${response.status}`);
+    }
+    const comments = await response.json();
+    const commentsList = document.querySelector(`#comment-list-${movieId}`);
+    commentsList.innerHTML = '';
+    comments.forEach((comment) => {
+      console.log(comment)
+      const commentMarkup = `
+        <div class="comment">
+          <p>${comment.creation_date}, ${comment.username} : ${comment.comment}</p>
+        </div>`;
+      commentsList.insertAdjacentHTML('beforeend', commentMarkup);
+    });
+  } catch (error) {
+    console.error('Error retrieving comments:', error);
+  }
+};
+
+const getCommentCount = async (movieId) => {
+  try {
+    const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/comments/?item_id=${movieId}`);
+    if (!response.ok) {
+      throw new Error(`Error fetching comments: ${response.status}`);
+    }
+    const comments = await response.json();
+    const commentCountElement = document.querySelector(`#comment-count-${movieId}`);
+    commentCountElement.textContent = comments.length;
+  } catch (error) {
+    console.error('Error retrieving comment count:', error);
+  }
+};
+
+
 
 const updateTotalLikes = () => {
   fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/SpsK74xULIr0Fmgge82L/likes/`)
@@ -87,18 +239,14 @@ const updateTotalLikes = () => {
       totalLikesElements.forEach((likesElement) => {
         const itemId = likesElement.getAttribute('id').split('-')[2];
         const itemLikes = likesData.find((item) => item.item_id === itemId);
-
-        if (itemLikes) {
-          const totalLikes = itemLikes.likes || 0;
-          likesElement.textContent = totalLikes;
-        } else {
-          likesElement.textContent = '0';
-        }
+        const totalLikes = itemLikes ? itemLikes.likes || 0 : 0;
+        likesElement.textContent = totalLikes;
       });
     })
     .catch((error) => {
       console.error('Error retrieving total likes:', error);
     });
 };
+
 
 export default getImage;
